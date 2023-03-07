aeb88718a49253978e2571cbe2fb7a17090266fc
Daniella-286 <119035286+Daniella-286@users.noreply.github.com>
2023-03-07 15:12:15 +0100

    s

--- FrontServlet.java
+++ FrontServlet.java
@@ -1,6 +1,8 @@
 package etu001914.framework.servlet;
 
 
+import etu001914.framework.Mapping;
+
 
 import java.io.*;
 import java.util.HashMap;
@@ -10,6 +12,23 @@ import javax.servlet.http.*;
 
 
 public class FrontServlet extends HttpServlet {
-    
+    HashMap<String,Mapping> mappinUrls;
+
+    public FrontServlet(HashMap<String, Mapping> mappinUrls) {
+        this.mappinUrls = mappinUrls;
+    }
+
+    public void setMappinUrls(HashMap<String, Mapping> mappinUrls) {
+        this.mappinUrls = mappinUrls;
+    }
+
+    public HashMap<String, Mapping> getMappinUrls() {
+        return mappinUrls;
+    }
+
+    public void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+    }
     
 }
